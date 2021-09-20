dbd07aa7aaf2143ae7c4e073cd537939ece84c1c
shivamparve0001 <shivamparve777@gmail.com>
2021-09-20 10:04:20 +0530

    1st Commit

---
+++ src/Nav.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav-black"}`}>
+      <img
+        className="nav-logo"
+        // src="logo link"
+        alt="netflix logo"
+      />
+      <img
+        className="nav-avatar"
+        // src="avatar link"
+        alt="login"
+      />
+    </div>
+  );
+}
+
+export default Nav;
